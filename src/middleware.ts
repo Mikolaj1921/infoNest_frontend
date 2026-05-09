17171d1f92049cba2a55af0b8182933b4573b68e
Mikolaj1921 <przteaml7@gmail.com>
2026-05-09 17:09:38 +0200

    feat: Update base global styles & some config files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
-// (Middleware routes) (task - 8)
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// eslint-disable-next-line
+export function middleware(request: NextRequest) {
+  // тимчасово скіп всі запити далі
+  return NextResponse.next();
+}
+
+// Налаштування рутів, які middleware має ігнорувати (поки тільки статичні файли)
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
