4887d3f7304775db16b6afc91026381fc8003cc2
Mikolaj1921 <przteaml7@gmail.com>
2026-05-12 12:46:24 +0200

    feat: Auth state archtecture - store config, user model

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,46 @@
+// ua: auth state architecture
+// стор для управління станом
+
+import { create } from 'zustand';
+import { User } from '@/types/user';
+
+// ua: інтерфейс для типізації стану авторизації
+
+interface AuthState {
+  user: User | null; // data user
+  isAuthenticated: boolean; // ua: чи користувач авторизований
+  isLoading: boolean; // ua: чи відбувається процес авторизації
+  setAuth: (user: User | null) => void; // ua: set користувача та статус авторизації
+  clearAuth: () => void; // ua: очистити дані авторизації
+  setLoading: (loading: boolean) => void; // ua: set статус завантаження
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  isLoading: true, // ua: true, поки перевіряється куки при першому старті
+
+  // сетери
+
+  // ua: сетер користувача та статусу авторизації
+  setAuth: (user) =>
+    set({
+      user,
+      isAuthenticated: true,
+      isLoading: false,
+    }),
+
+  // ua: сетер очиститки даних авторизації
+  clearAuth: () =>
+    set({
+      user: null,
+      isAuthenticated: false,
+      isLoading: false,
+    }),
+
+  // ua: сетер статусу завантаження
+  setLoading: (loading) =>
+    set({
+      isLoading: loading,
+    }),
+}));
---
+++ src/types/user.ts
@@ -0,0 +1,9 @@
+// ua: interface User - model of user data
+
+export interface User {
+  id: number;
+  email: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
