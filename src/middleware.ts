f484022aba156bb293094ccac234d227bc6cf139
Mikolaj1921 <przteaml7@gmail.com>
2026-05-22 10:24:51 +0200

    feat: Middleware configuration and redirect logic - setup global middleware - auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,35 @@
+// ua: import хуків та сервісів
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// eslint-disable-next-line
+// ua: Middleware для захисту priv-routes та перенаправлення auth-users з auth pages
 export function middleware(request: NextRequest) {
-  // тимчасово скіп всі запити далі
+  const { pathname } = request.nextUrl; // ua: поточний шлях
+
+  const token = request.cookies.get('accessToken')?.value; // token from cookies (subt 2)
+
+  //ua: визначення типів маршрутів
+  const isAuthPage = pathname === '/login' || pathname === '/register';
+  const isPrivatePage =
+    pathname.startsWith('/workspaces') || pathname.startsWith('/profile');
+
+  // ua: сценаріЇ
+  if (isPrivatePage && !token) {
+    const loginUrl = new URL('/login', request.url);
+    // save the current path
+    loginUrl.searchParams.set('from', pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if (isAuthPage && token) {
+    return NextResponse.redirect(new URL('/workspaces', request.url));
+  }
+
+  // skip middleware for other routes
   return NextResponse.next();
 }
 
-// Налаштування рутів, які middleware має ігнорувати (поки тільки статичні файли)
+// ua: matcher - шляхи на які буде діяти middleware (subt 3)
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/login', '/register', '/workspaces/:path*', '/profile/:path*'],
 };
